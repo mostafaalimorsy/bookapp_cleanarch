1831d72601900b9e952d6cdff77907589cea786b
mostafaalimorsy <mostafamorsy501@gmail.com>
2023-10-11 03:34:29 +0300

    feature Build

---
+++ lib/features/home/presentation/view/widget/featuerListView.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class FeatureListView extends StatelessWidget {
+  const FeatureListView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 24,
+      ),
+      child: SizedBox(
+        height: MediaQuery.sizeOf(context).height * .3,
+        child: ListView.separated(
+          physics: const BouncingScrollPhysics(),
+          scrollDirection: Axis.horizontal,
+          shrinkWrap: false,
+          itemBuilder: (BuildContext context, int index) {
+            return itemForBuild();
+          },
+          itemCount: 10,
+          separatorBuilder: (BuildContext context, int index) {
+            return const SizedBox(
+              width: 10,
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  AspectRatio itemForBuild() {
+    return AspectRatio(
+      aspectRatio: 2.7 / 4,
+      child: Container(
+        decoration: BoxDecoration(
+          // image: DecorationImage(image: AssetImage(""), fit: BoxFit.fill),
+          color: Colors.red,
+          borderRadius: BorderRadius.circular(25),
+        ),
+      ),
+    );
+  }
+}
--- lib/features/home/presentation/view/widget/homeViewBody.dart
+++ lib/features/home/presentation/view/widget/homeViewBody.dart
@@ -1,5 +1,7 @@
 import 'package:bookapp_cleanarch/features/home/presentation/view/widget/customAppBar.dart';
+import 'package:bookapp_cleanarch/features/home/presentation/view/widget/featuerListView.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({Key? key}) : super(key: key);
@@ -9,6 +11,7 @@ class HomeViewBody extends StatelessWidget {
     return const Column(
       children: [
         CustomAppBar(),
+        FeatureListView(),
       ],
     );
   }
