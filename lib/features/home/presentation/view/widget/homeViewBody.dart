3b6957c79dff65456582a0da63321002b6785987
mostafaalimorsy <mostafamorsy501@gmail.com>
2023-10-11 04:12:19 +0300

    const

--- lib/features/home/presentation/view/widget/homeViewBody.dart
+++ lib/features/home/presentation/view/widget/homeViewBody.dart
@@ -8,7 +8,7 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return const Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         CustomAppBar(),
