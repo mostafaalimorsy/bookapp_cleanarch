1e0cc29ec77f466e542ba66a2a87dd9b124c5dcd
mostafaalimorsy <mostafamorsy501@gmail.com>
2023-10-12 04:23:13 +0300

    cpmment the transaction

--- lib/core/const/const.dart
+++ lib/core/const/const.dart
@@ -1,6 +1,4 @@
 // ignore_for_file: constant_identifier_names
 
-import 'package:get/get_navigation/src/routes/transitions_type.dart';
-
 const KTranstationDuration = Duration(seconds: 2);
-const KTranstationAnimation = Transition.downToUp;
+// const KTranstationAnimation = Transition.downToUp;
