cea47477434cfc38f7ff58ccf95890f7bc46c4d2
mostafaalimorsy <mostafamorsy501@gmail.com>
2023-10-12 04:24:24 +0300

    serahc body ui

---
+++ lib/features/home/presentation/view/widget/searchScreen/searchbody.dart
@@ -0,0 +1,45 @@
+import 'package:bookapp_cleanarch/features/home/presentation/view/widget/homeWidget/bestSellerListView.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class Searchbody extends StatelessWidget {
+  const Searchbody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          searchAppBar(context),
+          BestSellerListView(
+            isSearch: true,
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget searchAppBar(BuildContext context) {
+    return SafeArea(
+      child: Row(
+        children: [
+          IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: const Icon(Icons.arrow_back),
+          ),
+          Container(
+            width: MediaQuery.sizeOf(context).width / 1.2,
+            child: TextFormField(
+              decoration: InputDecoration(
+                  hintText: "Search",
+                  suffixIcon: Icon(FontAwesomeIcons.magnifyingGlass),
+                  enabledBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(12))),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
