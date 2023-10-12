5bca2976f3ade40a4aa83bcf6ec1b38ff818bf54
mostafaalimorsy <mostafamorsy501@gmail.com>
2023-10-12 04:25:17 +0300

    book deatils

---
+++ lib/features/home/presentation/view/widget/deatilsScreenWidget/bookDeatils.dart
@@ -0,0 +1,33 @@
+import 'package:bookapp_cleanarch/features/home/presentation/view/widget/deatilsScreenWidget/deatilsBar.dart';
+import 'package:bookapp_cleanarch/features/home/presentation/view/widget/deatilsScreenWidget/deatilsItem.dart';
+import 'package:bookapp_cleanarch/features/home/presentation/view/widget/homeWidget/featuerListView.dart';
+import 'package:flutter/material.dart';
+
+import 'customDeatilsAppBar.dart';
+
+class BookDetails extends StatelessWidget {
+  const BookDetails({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const CustomDetailsAppBar(),
+        const DeatilsItem(),
+        const DeatilsBar(),
+        spesificText(),
+        SizedBox(
+          height: MediaQuery.of(context).size.height / 7,
+          child: const FeatureListView(),
+        ),
+      ],
+    );
+  }
+
+  Widget spesificText() {
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+      child: Align(alignment: Alignment.centerLeft, child: Text("You can also like")),
+    );
+  }
+}
