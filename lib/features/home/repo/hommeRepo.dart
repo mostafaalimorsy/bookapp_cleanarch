54133d0ce1800b658a2314cadeca5e09d3710d86
mostafaalimorsy <mostafamorsy501@gmail.com>
2023-10-14 05:13:18 +0300

    add either class

--- lib/features/home/repo/hommeRepo.dart
+++ lib/features/home/repo/hommeRepo.dart
@@ -1,10 +1,12 @@
 // ignore_for_file: file_names
 
 import 'package:bookapp_cleanarch/features/home/domin/entity/bookEntity.dart';
+import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
-  Future<List<BookEntity>> fetchFeaturedBooks();
-  Future<List<BookEntity>> fetchNewBooks();
-  Future<List<BookEntity>> fetchSearchBooks();
-  Future<List<BookEntity>> fetchDetailsBooks();
+  //to return fail if there is fail request that on left side and the right for success request using dartz package
+  Future<Either<Fail, List<BookEntity>>> fetchFeaturedBooks();
+  Future<Either<Fail, List<BookEntity>>> fetchNewBooks();
+  Future<Either<Fail, List<BookEntity>>> fetchSearchBooks();
+  Future<Either<Fail, List<BookEntity>>> fetchDetailsBooks();
 }
