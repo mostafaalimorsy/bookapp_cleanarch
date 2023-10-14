52cade1cd542325b64d70a9530edba645d9cdc0d
mostafaalimorsy <mostafamorsy501@gmail.com>
2023-10-14 05:03:56 +0300

    enities

---
+++ lib/features/home/domin/entity/bookEntity.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: file_names
+
+class BookEntity {
+  final String img;
+  final String title;
+  final String authName;
+  final num price;
+  final num rating;
+
+  BookEntity(this.img, this.title, this.authName, this.price, this.rating);
+}
