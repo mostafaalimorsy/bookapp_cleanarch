465fe072a16ab8785fe7e58ca640d1a9206155e7
mostafaalimorsy <mostafamorsy501@gmail.com>
2023-10-16 11:11:21 +0300

    create remote and local remote data source

--- lib/features/home/data/dataSource/local/local.dart
+++ lib/features/home/data/dataSource/local/local.dart
@@ -1,13 +1,13 @@
 import 'package:bookapp_cleanarch/features/home/domin/entity/bookEntity.dart';
 
-abstract class HomeRemoteDataSource {
+abstract class HomeLocalDataSource {
   Future<List<BookEntity>> fetchFeaturedBooks();
   Future<List<BookEntity>> fetchNewBooks();
   Future<List<BookEntity>> fetchSearchBooks();
   Future<List<BookEntity>> fetchDetailsBooks();
 }
 
-class HomeRemoteDataSourceImplement extends HomeRemoteDataSource {
+class HomeLocalDataSourceImplement extends HomeLocalDataSource {
   @override
   Future<List<BookEntity>> fetchDetailsBooks() {
     // TODO: implement fetchDetailsBooks
