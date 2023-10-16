b40683d3ee4ef6e0c7b033a64395ae49bfd4494d
mostafaalimorsy <mostafamorsy501@gmail.com>
2023-10-16 11:59:51 +0300

    generate adaptor using flutter packages pub run build_runner build

--- lib/features/home/domin/entity/bookEntity.dart
+++ lib/features/home/domin/entity/bookEntity.dart
@@ -1,11 +1,21 @@
 // ignore_for_file: file_names
 
+import 'package:hive/hive.dart';
+part 'bookEntity.g.dart';
+
+@HiveType(typeId: 0)
 class BookEntity {
+  @HiveField(0)
   final String bookId;
+  @HiveField(1)
   final String? img;
+  @HiveField(2)
   final String title;
+  @HiveField(3)
   final String? authorName;
+  @HiveField(4)
   final String? price;
+  @HiveField(5)
   final num? rating;
 
   BookEntity(
